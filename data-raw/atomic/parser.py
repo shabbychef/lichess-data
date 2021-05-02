49541733edee610f9d97eb5fa7acb943958694e8
Steven Pav <shabbychef@gmail.com>
2021-05-01 22:42:53 -0700

    makefile and code to pull and process atomic data

---
+++ data-raw/atomic/parser.py
@@ -0,0 +1,156 @@
+#! /usr/bin/env python
+# coding: utf-8
+#
+"""atomic pgn to csv conversion
+
+Usage:
+  parser.py INPGN OUTCSV
+
+"""
+
+from docopt import docopt
+
+
+def dpieces(endb):
+    """
+    return tuple of differences in piece counts
+    """
+    import chess
+    dPAWN = len(endb.pieces(chess.PAWN,chess.WHITE)) - len(endb.pieces(chess.PAWN,chess.BLACK))
+    dKNIGHT = len(endb.pieces(chess.KNIGHT,chess.WHITE)) - len(endb.pieces(chess.KNIGHT,chess.BLACK))
+    dBISHOP = len(endb.pieces(chess.BISHOP,chess.WHITE)) - len(endb.pieces(chess.BISHOP,chess.BLACK))
+    dROOK = len(endb.pieces(chess.ROOK,chess.WHITE)) - len(endb.pieces(chess.ROOK,chess.BLACK))
+    dQUEEN = len(endb.pieces(chess.QUEEN,chess.WHITE)) - len(endb.pieces(chess.QUEEN,chess.BLACK))
+    return (dPAWN,dKNIGHT,dBISHOP,dROOK,dQUEEN)
+
+def pgn_gen(pgn):
+    import chess.pgn
+    from re import sub,compile
+    rpat = compile("https?://lichess.org/")
+    yield ('site','datetime','termination','outcome','nply',
+            'move1','move2','move3','move4','move5','move6','move7','move8','move9','move10',
+            'white','black',
+            'white_elo','black_elo','white_elo_diff','black_elo_diff',
+            'l1_dpawn','l1_dknight','l1_dbishop','l1_drook','l1_dqueen',
+            'l2_dpawn','l2_dknight','l2_dbishop','l2_drook','l2_dqueen',
+            'l4_dpawn','l4_dknight','l4_dbishop','l4_drook','l4_dqueen',
+            'l8_dpawn','l8_dknight','l8_dbishop','l8_drook','l8_dqueen')
+    # empty
+    mt_dp = (float('nan'), float('nan'), float('nan'), float('nan'), float('nan'))
+    while (pgn):
+        gam = chess.pgn.read_game(pgn)
+        try:
+            rhed = gam.headers
+        except:
+            # cannot do anything without the headers? fake them!
+            #rhed = {'Termination':'ERROR','Result':'--','Site':"https://lichess.org/"}
+            # I believe you get here when you reach end of file. return then
+            break
+        term = rhed['Termination']
+        if term=='Normal':
+            moves = []
+            for iii in range(10):
+                try:
+                    gam = gam.next()
+                    moves.append(str(gam.move))
+                except:
+                    moves.append('')
+            try:
+                wdiff = int(rhed['WhiteRatingDiff'])
+                bdiff = int(rhed['BlackRatingDiff'])
+            except:
+                wdiff = float('nan')
+                bdiff = float('nan')
+            try:
+                ending = gam.end()
+                endb = ending.board()
+                nply = endb.ply()
+            except:
+                nply = float('nan')
+            try:
+                lmov = endb.pop()
+                d_l1 = dpieces(endb)
+            except:
+                d_l1 = mt_dp
+            try:
+                lmov = endb.pop()
+                d_l2 = dpieces(endb)
+            except:
+                d_l2 = mt_dp
+            try:
+                lmov = endb.pop()
+                lmov = endb.pop()
+                d_l4 = dpieces(endb)
+            except:
+                d_l4 = mt_dp
+            try:
+                lmov = endb.pop()
+                lmov = endb.pop()
+                lmov = endb.pop()
+                lmov = endb.pop()
+                d_l8 = dpieces(endb)
+            except:
+                d_l8 = mt_dp
+        else:
+            # could also be 'Time forfeit', 'Abandonded'
+            moves = ['','','','','','','','','','']
+            wdiff = float('nan')
+            bdiff = float('nan')
+            nply = float('nan')
+            d_l1 = mt_dp
+            d_l2 = mt_dp
+            d_l4 = mt_dp
+            d_l8 = mt_dp
+        # outcome from white's pov, as a number:
+        if rhed['Result'] == '1-0':
+            outcome = 1.0
+        elif rhed['Result'] == '0-1':
+            outcome = 0.0
+        elif rhed['Result'] == '1/2-1/2':
+            outcome = 0.5
+        else:
+            outcome = float('nan')
+        # who is playing
+        try:
+            white = rhed['White']
+            black = rhed['Black']
+        except:
+            white = ''
+            black = ''
+        # whie and black elo
+        try:
+            welo = int(rhed['WhiteElo'])
+        except:
+            welo = float('nan')
+        try:
+            belo = int(rhed['BlackElo'])
+        except:
+            belo = float('nan')
+
+        try:
+            dat = sub('\.','-',rhed['UTCDate'])
+            datetime = f"{dat}T{rhed['UTCTime']}Z"
+        except:
+            datetime = ''
+        # an ID for the game
+        site = sub(rpat,'',rhed['Site']) 
+        yield (site,datetime,term,outcome,nply,
+                *moves,
+                white,black,welo,belo,wdiff,bdiff,
+                *d_l1,*d_l2,*d_l4,*d_l8)
+    pgn.close()
+    return
+
+# module as script
+if __name__ == "__main__":
+    import csv
+    arguments = docopt(__doc__, version='parser.py 1.0')
+    pgn = open(arguments['INPGN'])
+    with open(arguments['OUTCSV'], 'w') as csvfile:
+        spamwriter = csv.writer(csvfile,delimiter=',', lineterminator='\n')
+        for result in pgn_gen(pgn):
+            spamwriter.writerow(result)
+        csvfile.close()
+
+#for vim modeline: (do not edit)
+# vim:ts=4:sw=4:sts=4:tw=79:sta:et:ai:nu:fdm=indent:syn=python:ft=python:tag=.py_tags;:cin:fo=croql
---
+++ data-raw/atomic/paste.r
@@ -0,0 +1,115 @@
+# /usr/bin/r
+#
+# Copyright 2021-2021 Steven E. Pav. All Rights Reserved.
+# Author: Steven E. Pav 
+#
+# This file is part of lichess.
+#
+# lichess is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# lichess is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with lichess.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Created: 2021.05.01
+# Copyright: Steven E. Pav, 2021
+# Author: Steven E. Pav <steven@gilgamath.com>
+# Comments: Steven E. Pav
+
+suppressMessages(library(docopt))       # we need docopt (>= 0.3) as on CRAN
+
+doc <- "Usage: paste.r [-v] [-O <OUTFILE>] INFILES...
+
+pastes together a bunch of atomic csv files, rejecting abnormal games, 
+and sorting by datetime.
+
+-O OUTFILE --outfile=OUTFILE     Give the outfile as csv [default: all_atomic.csv]
+-v --verbose                     Be more verbose
+-h --help                        show this help text"
+
+opt <- docopt(doc)
+
+suppressMessages({
+	library(readr)
+	library(dplyr)
+	library(tidyr)
+	library(magrittr)
+})
+
+readone <- function(apath) {
+  readr::read_csv(apath,col_types=cols(site = col_character(),
+																			 datetime = col_datetime(format = ""),
+																			 termination = col_character(),
+																			 outcome = col_double(),
+																			 nply = col_double(),
+																			 white_elo = col_double(),
+																			 black_elo = col_double(),
+																			 white_elo_diff = col_double(),
+																			 black_elo_diff = col_double(),
+																			 l1_dpawn = col_double(),
+																			 l1_dknight = col_double(),
+																			 l1_dbishop = col_double(),
+																			 l1_drook = col_double(),
+																			 l1_dqueen = col_double(),
+																			 l2_dpawn = col_double(),
+																			 l2_dknight = col_double(),
+																			 l2_dbishop = col_double(),
+																			 l2_drook = col_double(),
+																			 l2_dqueen = col_double(),
+																			 l4_dpawn = col_double(),
+																			 l4_dknight = col_double(),
+																			 l4_dbishop = col_double(),
+																			 l4_drook = col_double(),
+																			 l4_dqueen = col_double(),
+																			 l8_dpawn = col_double(),
+																			 l8_dknight = col_double(),
+																			 l8_dbishop = col_double(),
+																			 l8_drook = col_double(),
+																			 l8_dqueen = col_double(),
+																			 .default = col_character())) %>%
+    filter(termination=='Normal' ,
+           nzchar(move1) > 0,
+           move1 != 'NA',
+           !is.na(white_elo),
+           !is.na(black_elo)) %>%
+	arrange(datetime) 
+}
+
+output <- lapply(opt$INFILES,readone) %>%
+  bind_rows() %>%
+	arrange(datetime)
+
+playnum <- bind_rows(output %>% 
+										 rename(pid=white) %>%
+										 select(site,pid,datetime),
+										 output %>% 
+										 rename(pid=black) %>%
+										 select(site,pid,datetime)) %>%
+	arrange(datetime,pid) %>%
+	group_by(pid) %>%
+		mutate(gamenum=seq_len(n())) %>%
+	ungroup() %>%
+	select(-datetime)
+
+output <- output %>%
+	left_join(playnum %>% 
+						rename(white=pid,
+									 white_gamenum=gamenum),
+						by=c('site','white')) %>%
+	left_join(playnum %>% 
+						rename(black=pid,
+									 black_gamenum=gamenum),
+						by=c('site','black')) 
+
+output %>%
+	readr::write_csv(opt$outfile)
+
+#for vim modeline: (do not edit)
+# vim:fdm=marker:fmr=FOLDUP,UNFOLD:cms=#%s:syn=r:ft=r
